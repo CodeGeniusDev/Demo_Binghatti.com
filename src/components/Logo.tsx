bc4a5bf0c0f6e12bd07f27da7cbf5cea2a570df2
Code Genius <codegenius.inc@gmail.com>
2025-06-26 02:25:44 +0500

    almost done

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,26 +1,25 @@
-"use client"
-import React from "react"
+"use client";
+import React from "react";
 
-import LogoCarousel from "./ui/logo-carousel"
+import LogoCarousel from "./ui/logo-carousel";
 
 export function LogoCarouselDemo() {
   return (
-    <div className="space-y-8 py-18 px-12 bg-gray-600/80 backdrop-blur-sm">
+    <div className="space-y-8 py-18 px-4 sm:py-24 sm:px-4 md:py-24 md:px-12 lg:py-32 lg:px-20 bg-gray-600/80 backdrop-blur-sm flex justify-center items-center">
       <div className="w-full max-w-screen-lg mx-auto flex flex-col items-center space-y-8">
         <div className="text-center">
           <h1 className="text-4xl md:text-6xl font-bold text-white text-center">
             The best are already here
           </h1>
-          <a href="https://www.newcult.co" target="_blank">
-            <h1 className="text-4xl md:text-6xl font-bold text-white text-center">Join new cult</h1>
-          </a>
+          <h1 className="text-4xl md:text-6xl font-bold text-white text-center">
+            Join now
+          </h1>
         </div>
 
         <LogoCarousel columnCount={3} />
       </div>
     </div>
-  )
+  );
 }
 
-export default LogoCarouselDemo
-
+export default LogoCarouselDemo;
